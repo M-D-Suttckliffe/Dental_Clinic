123f3ca4350e0586728abaa2889a703abf62ed64
M-D-Suttckliffe <71319531+M-D-Suttckliffe@users.noreply.github.com>
2023-04-02 19:01:49 +0300

    Создал подлючение к бд на PostgreSQL

---
+++ Dental_Clinic/Context/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Numerics;
+
+namespace Dental_Clinic.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<MedServices> MedServices { get; set; }
+        public DbSet<ServicesProvided> ServicesProvideds { get; set; }
+        public DbSet<Visit> Visits { get; set; }
+        public DbSet<Medication> Medications { get; set; }
+        public DbSet<Diagnos> Diagnos { get; set; }
+        public DbSet<ListPrepforTreatment> ListPrepforTreatments { get; set; }
+        public DbSet<MedTreatment> MedTreatments { get; set; }
+    }
+}
